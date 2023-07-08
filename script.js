d374611792794f1f69157cdddd6dc23dfef2ddf2
shubham kanase <shubhamkanase92@gmail.com>
2023-07-08 23:53:46 +0530

    upadaten javascript

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const submit = document.querySelector("button");
+const submit = document.querySelector("#signup-form");
 const fname = document.querySelector("#fname");
 const lname = document.querySelector("#lname");
 const email = document.querySelector("#email");
 const pword = document.querySelector("#pword");
 const cpword = document.querySelector("#cpword");
 
-submit.addEventListener('click', ()=> {
+submit.addEventListener('submit', (event)=> {
+    event.preventDefault();
     if(pword.value != cpword.value){
+        cpword.setCustomValidity("Passwords Don't Match");
+        cpword.reportValidity();
+        cpword.style.borderColor="red";  
         alert("password not match")
+        
+
         // window.location.href="/second.html"
     }
     else{
-        alert("succefully nregister")
-        window.location.href="/second.html"
+        cpword.setCustomValidity(""); // Reset custom validity message
+        cpword.style.borderColor = ""; 
+        cpword.reportValidity(); // Update the validity state
+        
+        if (cpword.checkValidity()) {
+            alert("Successfully registered");
+            window.location.href = "/second.html";
+        // alert("succefully nregister")
+        }
     }
-  
-    // alert("succefully nregister")
 })
+
+// pword.onchange = validatePassword;
+// cpword.onkeyup = validatePassword;
